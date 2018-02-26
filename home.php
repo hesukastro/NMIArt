66c3eddf9c96b701b819b8430f2ae377c664fe33
Jesus Castro <joesei_castro@dlsu.edu.ph>
2018-02-26 23:38:37 +0800

    Created a temporary background for the home page. Also saved a psd file in the images folder if in case the client wants to keep it.

--- home.php
+++ home.php
@@ -5,8 +5,9 @@
     <!-- Site-wide dependencies -->
     <?php include 'templates/dependencies.php' ?>
     <!-- Page specific dependencies -->
+    <link rel="stylesheet" href="css\index.css">
 </head>
-<body>
+<body style="background: url('images/background.png') no-repeat center right fixed">
     <?php include 'templates/navBar.php' ?>
 </body>
 </html>
