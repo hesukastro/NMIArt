64d94cbbb4ae2652f9672dd0e30c266c13cdedfc
Jesus Castro <joesei_castro@dlsu.edu.ph>
2018-03-03 21:45:02 +0800

    Updated contacts page

--- contact.php
+++ contact.php
@@ -12,11 +12,21 @@
         <h2 class="center uppercase">Contact Us</h2>
         <div class="row">
             <div class="col-md-5 col-md-offset-1">
+                <div class="row">
+                    <div class="col-xs-12 icon-group">
+                        <span class="icon-text"><img class="icon" src="images/icons/instagram.svg"> @nadineibay</span>
+                    </div>
+                </div>
                 <form id="message-form" method="POST" action="phpScripts/contact.php">
-                    <div class="row mar-remove">
-                        <div class="form-group col-xs-12 col-md-6">
+                <div class="row mar-remove">
+                        <div class="form-group col-xs-12">
                             <input id="name" name="name" type="text" class="form-control" placeholder="Name" required>
                             <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                    <div class="row mar-remove">
+                        <div class="form-group col-xs-12 col-md-6">
+                            <input type="text" class="form-control" placeholder="Name" value="ibaynadine@gmail.com" disabled>
                         </div>
                         <div class="form-group col-xs-12 col-md-6">
                             <input id="email" name="email" type="email" class="form-control col-md-6" placeholder="Email" required>
@@ -25,7 +35,7 @@
                     </div>
                     <div class="row mar-remove">
                         <div class="form-group col-xs-12">
-                            <textarea id="message" name="message" class="form-control" rows="10" style="height:60%;" placeholder="Message" required></textarea>
+                            <textarea id="message" name="message" class="form-control" rows="10" style="height:50%;" placeholder="Message" required></textarea>
                             <button class="btn btn-dark btn-lg mar-top-md pull-right">Submit</button>
                         </div>
                     </div>
