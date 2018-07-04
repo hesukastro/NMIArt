6d09a529a35b52aa461e7a2db1472ef9390703bb
Jesus Castro <joesei_castro@dlsu.edu.ph>
2018-07-04 15:28:20 +0800

    updated the about photo and the collage

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
         <div class="row">
         <!-- <div class="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4"> -->
             <div class="col-xs-12 col-md-10 col-md-offset-1">
-                <img class="img-responsive" src="images/about2.jpg">
+                <img class="img-responsive img-800 center" src="images/about.jpg">
             </div>
             
             <div class="col-xs-12 col-md-10 col-md-offset-1 mar-top-lg">
--- home.php
+++ home.php
@@ -12,7 +12,7 @@
     <div class="container-fluid col-sm-offset-2">
         <div class="row">
             <div class="background-image col-xs-9 col-md-7 col-md-offset-1">
-                    <img src="/images/index/collage.png" height="100%">
+                    <img src="/images/index/collage.jpg" height="100%">
             </div>
         </div> <!-- End of "row" div -->
     </div> <!-- End of "container" div -->
